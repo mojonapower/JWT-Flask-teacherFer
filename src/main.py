90f1bdf1365eff50e178480890da1b8a41271ece
mojonapower <maca95@gmail.com>
2021-04-25 16:05:09 +0000

    jwt profe fernando

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ from admin import setup_admin
 from models import db, User
 #from models import Person
 
+#import JWT for tokenization
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -19,6 +22,10 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+# config for jwt
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -38,6 +45,59 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    if email is None:
+        return jsonify({"msg": "No email was provided"}), 400
+    if password is None:
+        return jsonify({"msg": "No password was provided"}), 400
+    
+    user = User.query.filter_by(email=email, password=password).first()
+    if user:
+        # the user was not found on the database
+        return jsonify({"msg": "User already exists"}), 401
+    else:
+        new_user = User()
+        new_user.email = email
+        new_user.password = password
+
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"msg": "User created successfully"}), 200
+    
+@app.route('/login', methods=['POST'])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    
+    if email is None:
+        return jsonify({"msg": "No email was provided"}), 400
+    if password is None:
+        return jsonify({"msg": "No password was provided"}), 400
+
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Invalid username or password"}), 401
+    else:
+        print(user)
+        # create a new token with the user id inside
+        access_token = create_access_token(identity=user.id)
+        return jsonify({ "token": access_token, "user_id": user.id }), 200
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    print(current_user_id, user)
+    return jsonify({"id": user.id, "email": user.email }), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
